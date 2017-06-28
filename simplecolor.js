c7c34623df6f687d0e77de0c729dc0a99d73b34a
jhnklly <jhnklly@gmail.com>
2017-06-28 13:23:47 -0700

    --amend

--- simplecolor.js
+++ simplecolor.js
@@ -12,7 +12,6 @@ d3.selectAll('.colorpicker .slider').on('input',function(d){
 
 d3.select('#hue').on('input',function(d){
   if ( Math.abs(newColor.h - this.value) >= TOLERANCE ) {
-    console.log(Math.abs(newColor.h - this.value));
     newColor.h = this.value;
     // console.log(newColor);
     d3.select('#new-color').style('background', newColor);
@@ -22,17 +21,21 @@ d3.select('#hue').on('input',function(d){
 });
 
 d3.select('#saturation').on('input',function(d){
-  newColor.s = this.value/100;
-  d3.select('#new-color').style('background', newColor);
-  d3.select('#saturation-number').attr('value',this.value);
-  updateSliders(newColor);
+  if ( Math.abs(newColor.s - this.value/100) >= TOLERANCE/100 ) {
+    newColor.s = this.value/100;
+    d3.select('#new-color').style('background', newColor);
+    d3.select('#saturation-number').attr('value',this.value);
+    updateSliders(newColor);
+  }
 });
 
 d3.select('#lightness').on('input',function(d){
-  newColor.l = this.value/100;
-  d3.select('#new-color').style('background', newColor);
-  d3.select('#lightness-number').attr('value',this.value);
-  updateSliders(newColor);
+  if ( Math.abs(newColor.l - this.value/100) >= TOLERANCE/100 ) {
+    newColor.l = this.value/100;
+    d3.select('#new-color').style('background', newColor);
+    d3.select('#lightness-number').attr('value',this.value);
+    updateSliders(newColor);
+  }
 });
 
 
@@ -42,45 +45,43 @@ d3.select('#hue')
 
 
 function updateSliders(d3color) {
-// Update hex code
-console.log(d3color);
-console.log(rgb2hex(d3color.toString()));
-d3.select('#new-hex').attr('value', rgb2hex(d3color.toString()) );
+  // Update hex code
+  d3.select('#new-hex').attr('value', rgb2hex(d3color.toString()) );
 
 
-// Ramp saturation from 0 to 100
-var fromColor, toColor, gradientColor;
-var satFrom = `hsla(${d3color.h}, 0%, ${100*d3color.l}%, 1)`;
-var satTo = `hsla(${d3color.h}, 100%, ${100*d3color.l}%, 1)`;
-var satGrad = `linear-gradient(to right, ${satFrom}, ${satTo})`;
-d3.select('#saturation').style('background', satGrad);
+  // Ramp saturation from 0 to 100
+  var fromColor, toColor, gradientColor;
+  var satFrom = `hsla(${d3color.h}, 0%, ${100*d3color.l}%, 1)`;
+  var satTo = `hsla(${d3color.h}, 100%, ${100*d3color.l}%, 1)`;
+  var satGrad = `linear-gradient(to right, ${satFrom}, ${satTo})`;
+  d3.select('#saturation').style('background', satGrad);
 
-// Ramp lightness from 0 to 100
-fromColor = `hsla(${d3color.h}, ${100*d3color.s}%, 0%, 1)`;
-midColor = `hsla(${d3color.h}, ${100*d3color.s}%, 50%, 1)`;
-toColor = `hsla(${d3color.h}, ${100*d3color.s}%, 100%, 1)`;
-gradientColor = `linear-gradient(to right, ${fromColor}, ${midColor}, ${toColor})`;
-d3.select('#lightness').style('background', gradientColor);
+  // Ramp lightness from 0 to 100
+  fromColor = `hsla(${d3color.h}, ${100*d3color.s}%, 0%, 1)`;
+  midColor = `hsla(${d3color.h}, ${100*d3color.s}%, 50%, 1)`;
+  toColor = `hsla(${d3color.h}, ${100*d3color.s}%, 100%, 1)`;
+  gradientColor = `linear-gradient(to right, ${fromColor}, ${midColor}, ${toColor})`;
+  d3.select('#lightness').style('background', gradientColor);
 
-// Ramp hue from 0 to 100
-fromColor = `hsla(0, ${100*d3color.s}%, ${100*d3color.l}%, 1)`;
-midColor = `hsla(90, ${100*d3color.s}%, ${100*d3color.l}%, 1)`;
-toColor = `hsla(180, ${100*d3color.s}%, ${100*d3color.l}%, 1)`;
-// gradientColor = `linear-gradient(to right, ${fromColor}, ${midColor}, ${toColor})`;
-gradientColor = "linear-gradient(to right ";
-for (var i = 0; i <= 360; i = i + 10) {
-  gradientColor += `, hsla(${i}, ${100*d3color.s}%, ${100*d3color.l}%, 1)`;
-}
-gradientColor += ") ";
-d3.select('#hue').style('background', gradientColor);
+  // Ramp hue from 0 to 100
+  fromColor = `hsla(0, ${100*d3color.s}%, ${100*d3color.l}%, 1)`;
+  midColor = `hsla(90, ${100*d3color.s}%, ${100*d3color.l}%, 1)`;
+  toColor = `hsla(180, ${100*d3color.s}%, ${100*d3color.l}%, 1)`;
+  // gradientColor = `linear-gradient(to right, ${fromColor}, ${midColor}, ${toColor})`;
+  gradientColor = "linear-gradient(to right ";
+  for (var i = 0; i <= 360; i = i + 10) {
+    gradientColor += `, hsla(${i}, ${100*d3color.s}%, ${100*d3color.l}%, 1)`;
+  }
+  gradientColor += ") ";
+  d3.select('#hue').style('background', gradientColor);
 
 }
 
 function rgb2hex(rgb){
-// ignores alpha
-rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
-return (rgb && rgb.length === 4) ? "#" +
-  ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
-  ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
-  ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
+  // ignores alpha
+  rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+  return (rgb && rgb.length === 4) ? "#" +
+    ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+    ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
+    ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
 }
