a07f4fa87292d1f679333d8b6ba18afdcfa4f113
jhnklly <jhnklly@gmail.com>
2017-06-29 14:26:22 -0700

    Add opacity, checkerboard. Sync numeric controls. Cleanup.

--- simplecolor.js
+++ simplecolor.js
@@ -17,7 +17,7 @@ function simplecolor(pickerSelector, targetSelector) {
         ;
 
     var newColor = d3.hsl("#00AAFF");
-    updateSliders(newColor);
+    newColor.opacity = 0.5; // todo - sync
 
     if (!d3.hsl(oldColorHex).h) oldColorHex = "#cccccc";
     newColorObj = d3.hsl(oldColorHex);
@@ -51,22 +51,47 @@ function simplecolor(pickerSelector, targetSelector) {
         ;
 
     var scSliders = scRows
-        .append('input')
-            .attr('class', 'sc-slider sc-grow')
-            .attr('id', (d) => d)
-            .attr('type', 'range')
-            .attr('value', 50)
-            .attr('step', TOLERANCE)
+        .append('div')
+            .attr('id', (d) => {
+                return "sc-slider-container-" + d;
+            })
+            .append('input')
+                .attr('class', 'sc-slider sc-grow')
+                .attr('id', (d) => {
+                    return "sc-" + d;
+                })
+                .attr('type', 'range')
+                .attr('value', 50)
+                .attr('step', TOLERANCE)
+        ;
+    // Override range for hue slider
+    d3.select('.sc-slider#sc-hue')
+        .attr('max',360)
+        .attr('value',200)
+        ;
+    // Override background for opacity slider
+    d3.select('.sc-slider#sc-alpha')
+        .attr('max',1)
+        .attr('value',0.5)
+        .attr('step',0.1)
+        .classed("checkerboard", true);
         ;
 
     scRows
         .append('input')
             .attr('class', 'sc-number')
             .attr('id', (d) => {
-                return d + "-number";
+                return "sc-" + d + "-number";
             })
             .attr('type', 'text')
             .attr('value', 50)
+        ;
+    // Override numbers
+    d3.select('#sc-hue-number')
+        .attr('value',200)
+        ;
+    d3.select('#sc-alpha-number')
+        .attr('value', 0.5)
         ;
 
     scRows
@@ -77,62 +102,126 @@ function simplecolor(pickerSelector, targetSelector) {
             })
         ;
 
+    // swatches & hex code
+    var scSwatchRow = sccp.append('div')
+        .attr('class','sc-flex-row')
+        ;
+    scSwatchRow.append('div')
+        .attr('class','sc-swatch checkerboard')
+        .append('div')
+            .attr('id','sc-old-color')
+            .attr('class','sc-swatch')
+        ;
+    scSwatchRow.append('div')
+        .attr('class','sc-swatch checkerboard')
+        .append('div')
+            .attr('id','sc-new-color')
+            .attr('class','sc-swatch')
+        ;
+    scSwatchRow.append('div')
+        .append('input')
+            .attr('id','sc-new-hex')
+            .on('input', function(){
+                // Use this value to update the sliders
+                console.log(this.value, d3.color(this.value));
+                if ( d3.color(this.value) ) {
+                    // updateSliders(this.value);
+                    updateInputs(this.value);
+                }
+            })
+        ;
+    scSwatchRow.append('div')
+        .append('input')
+            .attr('id','sc-new-rgba')
+        ;
+
+    updateSliders(newColor);
+
+    function updateInputs(colorString) {
+        d3color = d3.hsl(colorString);
+        console.log('updateInputs',d3color);
+        d3.select('#sc-hue').attr('value', d3color.h);
+        d3.select('#sc-saturation').attr('value', d3color.s);
+        d3.select('#sc-lightness').attr('value', d3color.l);
+        d3.select('#sc-alpha').attr('value', d3color.opacity);
+        updateSliders(d3color);
+
+        d3.select('#sc-hue-number').attr('value', d3color.h);
+        d3.select('#sc-saturation-number').attr('value', d3color.s);
+        d3.select('#sc-lightness-number').attr('value', d3color.l);
+        d3.select('#sc-alpha-number').attr('value', d3color.opacity);
+    }
 
     // Define the colorChange function
     function updateSliders(d3color) {
-        // console.log(d3color);
         // Update hex code && target
-        d3.select('#new-hex').attr('value', rgb2hex(d3color.toString()) );
-        target.style('background', '#00aaff');
+        d3.select('#sc-new-color').style('background', d3.hsl(d3color).toString() );
+        d3.select('#sc-new-hex').attr('value', rgb2hex(d3color.toString()) );
+        d3.select('#sc-new-rgba').attr('value', d3.rgb(d3color).toString().replace(/ /g,"") );
+        target.style('background', rgb2hex(d3color.toString()) );
 
         // Ramp hue from 0 to 100
         fromColor = `hsla(0, ${100*d3color.s}%, ${100*d3color.l}%, 1)`;
         midColor = `hsla(90, ${100*d3color.s}%, ${100*d3color.l}%, 1)`;
         toColor = `hsla(180, ${100*d3color.s}%, ${100*d3color.l}%, 1)`;
-        d3.select('#hue').style('background', makeGradient(d3color.toString()));
+        d3.select('#sc-hue').style('background', makeGradient(d3color.toString()));
 
         // Ramp saturation from 0 to 100
         fromColor = `hsla(${d3color.h}, 0%, ${100*d3color.l}%, 1)`;
         // midColor = `hsla(${d3color.h}, ${100*d3color.s}%, 50%, 1)`;
         toColor = `hsla(${d3color.h}, 100%, ${100*d3color.l}%, 1)`;
         gradientColor = `linear-gradient(to right, ${fromColor}, ${toColor})`;
-        d3.select('#saturation').style('background', gradientColor);
+        d3.select('#sc-saturation').style('background', gradientColor);
 
         // Ramp lightness from 0 to 100
         fromColor = `hsla(${d3color.h}, ${100*d3color.s}%, 0%, 1)`;
         midColor = `hsla(${d3color.h}, ${100*d3color.s}%, 50%, 1)`;
         toColor = `hsla(${d3color.h}, ${100*d3color.s}%, 100%, 1)`;
         gradientColor = `linear-gradient(to right, ${fromColor}, ${midColor}, ${toColor})`;
-        d3.select('#lightness').style('background', gradientColor);
+        d3.select('#sc-lightness').style('background', gradientColor);
+
+        // Ramp alpha from 0 to 100
+        fromColor = `hsla(${d3color.h}, ${100*d3color.s}%, ${100*d3color.l}%, 0)`;
+        toColor = `hsla(${d3color.h}, ${100*d3color.s}%, ${100*d3color.l}%, 1)`;
+        gradientColor = `linear-gradient(to right, ${fromColor}, ${toColor})`;
+        d3.select('#sc-alpha').style('background', gradientColor);
 
     }
 
     // Set the on change handlers
-
-    d3.select('#hue').on('input',function(d){
+    d3.select('#sc-hue').on('input',function(d){
         if ( Math.abs(newColor.h - this.value) >= TOLERANCE ) {
             newColor.h = this.value;
-            // console.log(newColor);
-            d3.select('#new-color').style('background', newColor);
-            d3.select('#hue-number').attr('value',this.value);
+            console.log(newColor, d3.hsl(newColor).toString() );
+            d3.select('#sc-new-color').style('background', d3.hsl(newColor).toString() );
+            d3.select('#sc-hue-number').attr('value',this.value);
             updateSliders(newColor);
         }
     });
 
-    d3.select('#saturation').on('input',function(d){
+    d3.select('#sc-saturation').on('input',function(d){
         if ( Math.abs(newColor.s - this.value/100) >= TOLERANCE/100 ) {
             newColor.s = this.value/100;
-            d3.select('#new-color').style('background', newColor);
-            d3.select('#saturation-number').attr('value',this.value);
+            d3.select('#sc-new-color').style('background', newColor);
+            d3.select('#sc-saturation-number').attr('value',this.value);
             updateSliders(newColor);
         }
     });
 
-    d3.select('#lightness').on('input',function(d){
+    d3.select('#sc-lightness').on('input',function(d){
         if ( Math.abs(newColor.l - this.value/100) >= TOLERANCE/100 ) {
             newColor.l = this.value/100;
-            d3.select('#new-color').style('background', newColor);
-            d3.select('#lightness-number').attr('value',this.value);
+            d3.select('#sc-new-color').style('background', newColor);
+            d3.select('#sc-lightness-number').attr('value',this.value);
+            updateSliders(newColor);
+        }
+    });
+
+    d3.select('#sc-alpha').on('input',function(d){
+        if ( Math.abs(newColor.opacity - this.value) >= TOLERANCE/100 ) {
+            newColor.opacity = this.value;
+            d3.select('#sc-new-color').style('background', newColor);
+            d3.select('#sc-alpha-number').attr('value',this.value);
             updateSliders(newColor);
         }
     });
@@ -140,16 +229,14 @@ function simplecolor(pickerSelector, targetSelector) {
 
     function makeGradient(hex) {
         var colorArr = [];
-        var d3c = d3.hcl(hex);
-        var hcl;
-        // Increment through the d3.hcl spectrum
+        var d3c = d3.hsl(hex);
+        // Increment through the d3.hcl(?) spectrum
         // Converting each to HSL for CSS
         for (var i = 0; i <= 360; i = i + 10) {
             d3c.h = i;
             colorArr.push(d3c.toString());
         }
         var ret = `linear-gradient(to right, ${colorArr.join(",")} )`;
-        console.log(ret);
         return ret;
     }
 
